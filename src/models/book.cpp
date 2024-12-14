906d5c5ac8f0f414725b8c1c269f87e151fb547d
Navid Aminnia <aminnia.navid@gmail.com>
2024-12-14 20:27:14 +0100

    add book class

--- src/models/book.cpp
+++ src/models/book.cpp
@@ -1,6 +1,62 @@
 #include "book.h"
 
-book::book()
+Book::Book(const std::string& title, const std::string& author, int year)
+    : bookId(0),  // Will be set by Library class
+      title(title),
+      author(author),
+      year(year),
+      available(true),
+      currentBorrowerId(-1)  // -1 indicates no borrower
 {
+}
 
+// Getters
+int Book::getBookId() const {
+    return bookId;
+}
+
+std::string Book::getTitle() const {
+    return title;
+}
+
+std::string Book::getAuthor() const {
+    return author;
+}
+
+int Book::getYear() const {
+    return year;
+}
+
+// Checks if the book is available
+bool Book::isAvailable() const {
+    return available;
+}
+
+// Retrieves the current Borrwers's ID
+int Book::getCurrentBorrowerId() const {
+    return currentBorrowerId;
+}
+
+// Setters
+void Book::setId(int id) {
+    bookId = id;
+}
+
+void Book::setAvailability(bool status) {
+    available = status;
+}
+
+void Book::setBorrowerId(int userId) {
+    currentBorrowerId = userId;
+    available = (userId == -1);  // Book is available if there's no borrower
+}
+
+// Utility methods
+std::string Book::toString() const {
+    return "Book ID: " + std::to_string(bookId) +
+           "\nTitle: " + title +
+           "\nAuthor: " + author +
+           "\nYear: " + std::to_string(year) +
+           "\nStatus: " + (available ? "Available" : "Borrowed") +
+           (currentBorrowerId != -1 ? "\nBorrowed by user: " + std::to_string(currentBorrowerId) : "");
 }
--- src/models/book.h
+++ src/models/book.h
@@ -1,11 +1,32 @@
-#ifndef BOOK_H
-#define BOOK_H
+#pragma once
+#include <string>
 
+class Book {
+private:
+    int bookId;
+    std::string title;
+    std::string author;
+    int year;
+    bool available;
+    int currentBorrowerId;
 
-class book
-{
 public:
-    book();
-};
+    // Constructor
+    Book(const std::string& title, const std::string& author, int year);
 
-#endif // BOOK_H
+    // Getters
+    int getBookId() const;
+    std::string getTitle() const;
+    std::string getAuthor() const;
+    int getYear() const;
+    bool isAvailable() const;
+    int getCurrentBorrowerId() const;
+
+    // Setters
+    void setId(int id);
+    void setAvailability(bool status);
+    void setBorrowerId(int userId);
+
+    // Utility methods
+    std::string toString() const;
+};
